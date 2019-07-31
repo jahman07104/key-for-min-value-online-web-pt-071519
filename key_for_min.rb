ac2ffe77e763db378432ee4ab3839dbc1c99e7cc
Patrick Harrison <jahman@bellatlantic.net>
2019-07-31 21:20:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
  #{:blake=>500, :ashley=>2, :adam=>1} to work on and return the lowest key based on the value => :adam}
       min_key = nil
       min_value = nil
-     hash.each do |key,value|
+     name_hash.each do |key,value|
         if min_value == nil || min_value > value
          min_value = value
          min_key = key
